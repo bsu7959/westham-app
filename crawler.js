503663df8936cd1133186941de07c6c5955fa8ac
bsu7959 <bsu7959@gmail.com>
2024-03-07 15:35:52 +0900

    기사 내용 크롤링 함수 작성 완료

--- crawler.js
+++ crawler.js
@@ -17,9 +17,9 @@ const request = async (presentPage) => {
             articles.push({
                 title: a.firstElementChild.textContent.trim(),
                 link: a.firstElementChild.href,
-                img: a.firstElementChild.nextElementSibling.firstElementChild.firstElementChild.src,
-                date: a.firstElementChild.nextElementSibling.nextElementSibling.firstElementChild.nextElementSibling.lastElementChild.textContent.trim(),
-                content: '',
+                img: a.querySelector('img').src,
+                date: a.querySelector('.m-teaser__content-info .m-teaser__content-publish-date').textContent.trim(),
+                content: [],
             });
         });
         return articles;
@@ -27,5 +27,59 @@ const request = async (presentPage) => {
     return articles;
 }
 
+const contentRequest = async (article) => {
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
+    await page.setViewport({
+        width: 1900,
+        height: 5000
+    });
+    await page.goto(article.link);  // 나중에 지울 것
 
-export { request };
+    const content = await page.evaluate(() => {
+        const content = [];
+        document.querySelectorAll('article .m-article__content').forEach((a) => {
+            let item = a.firstElementChild;
+
+            while(item) {
+                console.log(item.tagName);
+                switch(item.tagName) {
+                    case 'DIV':
+                        if(item.classList[0] == 'paragraph') break;
+                        item.querySelectorAll('.field__item p').forEach((a) => {
+                            content.push(a.textContent); 
+                        })
+                        break;
+                    case 'FIGURE':
+                        item.querySelectorAll('img').forEach((a) => {
+                            content.push(a.outerHTML);
+                        })
+                        break;
+                    default:
+                        break;
+                }
+                
+                item = item.nextElementSibling;
+            }
+            // a.querySelectorAll('.field__item').forEach((a) => {
+            //     console.log(a);
+            // })
+            // a.querySelectorAll('figure img').forEach((a) => {
+            //     console.log(a.src)
+            // })
+            // console.log(content)
+
+        });
+
+        return content;
+    });
+    return {
+        title: article.title,
+        link: article.link,
+        img: article.img,
+        date: article.date,
+        content: content,
+    };
+}
+
+export { request, contentRequest };
--- server.js
+++ server.js
@@ -1,13 +1,20 @@
-// const request = require('./crawler.js')
-// const express = require('express');
-// const path = require('path');
+
 import express from 'express';
 import path from 'path';
-import { request } from './crawler.js';
+import { request, contentRequest } from './crawler.js';
 const app = express();
 const PORT = 8080;
-const articles = await request(0);
-console.log(articles)
+//const articles = await request(0);
+//console.log(articles);
+const content = await contentRequest({
+    title: 'Iron Cast | Lee Bowyer on fulfilling his childhood dream, returning to the Hammers and managing Montserrat',
+    link: 'https://www.whufc.com/news/iron-cast-lee-bowyer-fulfilling-his-childhood-dream-returning-hammers-and-managing-montserrat',
+    img: 'https://cdn.whufc.com/sites/default/files/2024-03/bowyerpodcast.jpg',
+    date: '5th March 2024',
+    
+})
+console.log(content);
+
 app.listen(PORT, function() {
     console.log(`listening on ${PORT}`);
 });
